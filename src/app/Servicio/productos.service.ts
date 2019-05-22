9c80a54b1c9b00561427bfdd73e5ef3cb15202ef
Agorex <matias_123@live.cl>
2019-05-22 16:58:32 -0400

    v1.1

--- src/app/Servicio/productos.service.ts
+++ src/app/Servicio/productos.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+      const url =  `https://testapi.io/api/hilderh/products/home`;
+
+      return this.http.get(url);
+  }
+
+  getProductos(){
+    return this.getQuery().pipe(map(data.items));
+  }
+
+
+
+
 }
